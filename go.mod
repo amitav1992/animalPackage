d71af13dadd21046530fdf918149e817e6eb96e5
amitav1992 <ghoshamit@live.com>
2024-01-20 19:52:40 +0530

    added depend mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/amitav1992/animalPackage
 
 go 1.21.6
+
+require github.com/amitav1992/puppy v0.0.0-20240120140359-36efa4bf304a // indirect
---
+++ mod-code-depe/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/amitav1992/puppy"
+)
+
+func main() {
+
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+
+}
